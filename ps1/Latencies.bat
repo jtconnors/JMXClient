6d9f4d94481ee96b459f1ccd8bf7073c093c22c6
jtconnors <git@jtconnors.com>
2022-08-31 14:26:29 -0400

    Removed old .bat files

--- ps1/Latencies.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-call incljava.bat
-
-set CLASSPATH_ARGS=-cp JMXClient.jar
-set MAIN_CLASS=com.example.latencies.Latencies
-set OTHER_ARGS=
-
-@echo on
-%JAVA_EXE% %JVM_OPTS% %JFR_OPTS% %CLASSPATH_ARGS% %OTHER_ARGS% %MAIN_CLASS%
